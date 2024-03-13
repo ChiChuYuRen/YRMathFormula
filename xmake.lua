20d4304e2323a76b792c8772358199e389d4d06e
ChiChuYuRen <chichuyuren@163.com>
2024-03-13 21:08:36 +0800

    xmake工程文件

---
+++ xmake.lua
@@ -0,0 +1,35 @@
+set_project("YRMathFormula")
+
+add_rules("mode.debug", "mode.release")
+
+set_languages("c++20")
+if is_plat("windows") then
+    if is_mode("debug") then
+        set_runtimes("MDd")
+        add_requires("qtadvanceddocking","MircoTex",  { debug = is_mode("debug") })
+    else
+        set_runtimes("MD")        
+        add_requires("qtadvanceddocking","MircoTex")
+    end
+else 
+    add_requires("qtadvanceddocking","MircoTex",{ debug = is_mode("debug") })
+end
+
+if is_plat("windows") then
+    set_optimize("fastest")  -- faster: -O2  fastest: -Ox  none: -O0
+    add_cxflags("/EHsc")
+    add_ldflags("/SAFESEH:NO")
+elseif is_plat("mingw") then
+    add_ldflags("-static-libgcc -static-libstdc++ -Wl,-Bstatic -lstdc++ -lwinpthread -Wl,-Bdynamic", {force = true})
+    set_optimize("faster")
+else
+    --set_optimize("none")   -- faster: -O2  fastest: -O3  none: -O0
+    set_optimize("faster")   -- faster: -O2  fastest: -O3  none: -O0
+    --add_cxflags("-Wno-narrowing", "-Wno-sign-compare", "-Wno-strict-aliasing")
+    if is_plat("macosx", "iphoneos") then
+        add_cxflags("-fno-pie")
+    end
+end
+
+includes("3rd","src")
+
